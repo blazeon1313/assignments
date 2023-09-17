36c291ad0788e826059c99c405983967020228ac
Kristoffer Krueger <kristoffer.krueger@bryanuniversity.edu>
2023-09-17 14:53:20 -0600

    week1

---
+++ recall.py
@@ -0,0 +1,24 @@
+class Recall:
+    def __init__(self):
+        self.data = []
+
+    def addNumber(self, num):
+        self.data.append(num)
+
+    def addString(self, fire):
+        self.data.append(fire)
+
+    def addDictionary(self, work):
+        self.data.append(work)
+
+    def addTuple(self, new):
+        self.data.append(new)
+
+
+newList = Recall()
+newList.addNumber(77)
+newList.addString("Firefighting is a passion of mine.")
+newList.addDictionary({"profession": "Firefighter"})
+newList.addTuple(("new fire", "hard work ahead"))
+
+print(newList.data)
